6341e88e6d404a009ee7402ade697bbcfb854ae5
Romain Fromi <romain.fromi@gmail.com>
2021-04-09 16:29:01 +0200

    Improve rules files and add Fullscreen dialog and missing translations

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,4 @@
-import {FailuresDialog,Menu, useGame} from '@gamepark/react-client'
+import {FailuresDialog, FullscreenDialog, Menu, useGame} from '@gamepark/react-client'
 import {DndProvider} from 'react-dnd-multi-backend'
 import HTML5ToTouch from 'react-dnd-multi-backend/dist/cjs/HTML5toTouch'
 import GameDisplay from './GameDisplay'
@@ -15,6 +15,7 @@ function App() {
             {game && <GameDisplay game={game}/>}
             <Menu/>
             <FailuresDialog/>
+            <FullscreenDialog/>
         </DndProvider>
     )
 }
