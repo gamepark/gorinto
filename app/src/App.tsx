23c01c2084a47dddb041741ca718793aeaa35917
Tho33 <theo.gregorio@laposte.net>
2021-04-22 16:45:53 +0200

    Ajout composant LoadingScreen

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,11 +4,13 @@ import HTML5ToTouch from 'react-dnd-multi-backend/dist/cjs/HTML5toTouch'
 import GameDisplay from './GameDisplay'
 import GameView from "@gamepark/gorinto/types/GameView";
 import useHeaderText from "./images/useHeaderText";
-import {Header} from "@gamepark/react-components";
-import { useState } from 'react';
+import {Header, LoadingScreen} from "@gamepark/react-components";
+import { useEffect, useState } from 'react';
 import SoundLoader from './SoundLoader';
 import FirstPlayerSound from './sounds/gongv1.mp3'
 import MoveTileSound from './sounds/tic.mp3'
+import GorintoBox from './images/gorintoBox.png'
+import { css } from '@emotion/react';
 
 
 function App() {
@@ -16,12 +18,20 @@ function App() {
     const headerText = useHeaderText()
 
     const [isSoundsLoading, setSoundLoading] = useState(true)
-    const loadingSounds = isSoundsLoading
+    const [isJustDisplayed, setJustDisplayed] = useState(true)
+    useEffect(() => {
+      setTimeout(() => setJustDisplayed(false), 2000)
+    }, [])
+
+    const loading = !game || isSoundsLoading || isJustDisplayed
+
+    console.log(loading)
 
     return (
         <DndProvider options={HTML5ToTouch}>
             <Header> {headerText}</Header>
-            {game && !loadingSounds && <GameDisplay game={game}/>}
+            <LoadingScreen gameBox={GorintoBox} author="Richard Yaner" artist="Josh Cappel" publisher={["Grand Games Guild","Super Meeple"]} display={loading} css={loadingStyle} />
+            {!loading && <GameDisplay game={game!}/>}
             <SoundLoader sounds={[FirstPlayerSound, MoveTileSound]} onSoundLoad={() => setSoundLoading(false)} />
             <Menu/>
             <FailuresDialog/>
@@ -29,5 +39,15 @@ function App() {
         </DndProvider>
     )
 }
+
+const loadingStyle = css`
+opacity:1;
+text-align:center;
+margin;auto auto;
+p, h2{
+    opacity:0.999;
+    color:black;
+}
+`
 
 export default App
